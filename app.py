6dfbd7271649e42216306dc9a52d84474af0589a
epsar-co <emilio.pariente@scsalud.es>
2025-09-24 18:20:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -133,9 +133,11 @@ st.markdown(
 )
 
 st.caption(
-    'References: Pariente et al, 'Fast Ossifier in DISH' RMD Open, sept 2025: https://doi.org/10.1136/rmdopen-2025-006024',
-     'Fast Ossifier Stratification Index (FOSSI): A propensity tool-derived tool in DISH' '(Sept 2025, submitted)',
-    
-This tool provides research-oriented risk stratification and does not replace clinical judgement.
-No data are transmitted off your browser in the Streamlit Cloud deployment.
+    "References: Pariente et al., 'Fast Ossifier in DISH.' RMD Open, Sept 2025. "
+    "https://doi.org/10.1136/rmdopen-2025-006024 | "
+    "'Fast Ossifier Stratification Index (FOSSI): A propensity tool-derived tool in DISH' "
+    "(Sept 2025, submitted).\n\n"
+    "This tool provides research-oriented risk stratification and does not replace clinical judgment. "
+    "No data are transmitted off your browser in the Streamlit Cloud deployment."
 )
+
